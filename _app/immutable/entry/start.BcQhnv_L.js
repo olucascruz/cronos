88e79e0042fc9c99c99dfe33e530d471a6ec0a85
LucasCruz <lca.snf21@uea.edu.br>
2025-08-23 09:09:35 -0400

    Updates

--- _app/immutable/entry/start.BcQhnv_L.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BKck3GBM.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.C61AzuuV.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/lIe0l8p1.js";export{o as load_css,r as start};
